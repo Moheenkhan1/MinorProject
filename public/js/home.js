6c8ea9777be4db7f2ab470668950905fcd321510
abhii-dev <abhiabhishek2504@gmail.com>
2024-11-21 22:48:20 +0530

    scroll-bar

--- public/js/home.js
+++ public/js/home.js
@@ -50,28 +50,37 @@ const backBtn = document.getElementById("backBtn");
 const nextBtn = document.getElementById("nextBtn");
 
 nextBtn.addEventListener("click", () => {
+    const quoteWidth = document.querySelector(".quote1").offsetWidth;
+    const gap = parseInt(getComputedStyle(scrollContainer).gap) || 0; 
     scrollContainer.style.scrollBehavior = "smooth";
-    scrollContainer.scrollLeft += 900;
-     
+    scrollContainer.scrollLeft += quoteWidth + gap; 
 });
 
 backBtn.addEventListener("click", () => {
+    const quoteWidth = document.querySelector(".quote1").offsetWidth;
+    const gap = parseInt(getComputedStyle(scrollContainer).gap) || 0; 
     scrollContainer.style.scrollBehavior = "smooth";
-    scrollContainer.scrollLeft -= 900; 
+    scrollContainer.scrollLeft -= quoteWidth + gap; 
 });
+/*
+getComputedStyle(scrollContainer) retrives all the styles applied to the scroll container. .gap property returns the value of the gap property. If the gap property is not set, it returns 0.
+the parseInt () function converts the string returned by getComputedStyle() to an integer. The || 0 is a default value in case the gap property is not set.
+the || provides a fallback value If parseInt results in NaN (e.g., if the gap property is not set or invalid), the value defaults to 0.
+*/
+
 
 
 
 //js for adding to cart in scrollable Menu
 
-let basket = JSON.parse(localStorage.getItem("data")) || [];
+/*let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 let calculation = () => {
     let cartIcon = document.querySelector(".cart-value");
     cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
   };
   
-  calculation();
+  calculation();*/
 
 // let itemContainer = ``;
 //        // generating html
